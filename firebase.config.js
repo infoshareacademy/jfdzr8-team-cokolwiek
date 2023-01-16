2f49eac431f577cf5d7c29ebda4516bcdf9f42ff
WIKTOR SOBCZAK <102958796+WiktorSobczak@users.noreply.github.com>
2023-01-16 12:47:44 +0100

    extracted firebase.config.js from the gitignore

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBt3pLHKyAC7gDr73JWF9O4_ZxUYjhU24g",
+    authDomain: "timetaker-6f355.firebaseapp.com",
+    projectId: "timetaker-6f355",
+    storageBucket: "timetaker-6f355.appspot.com",
+    messagingSenderId: "500649280511",
+    appId: "1:500649280511:web:5cc1880cd9a28242be5dca",
+    measurementId: "G-62G098JT6Y"
+  };
