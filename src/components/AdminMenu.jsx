fea0a6fd63e8809d1f8f26bb1fd4943cdc54a0d5
Gal Anonim <ssosnkrz@gmail.com>
2023-01-21 19:16:44 +0100

    admin menu

--- src/components/AdminMenu.jsx
+++ src/components/AdminMenu.jsx
@@ -1,3 +1,39 @@
-export const AdminMenu = () => {
-    return (<h1>Admin Menu</h1>)
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+import { addLocation } from "../firebase/utils/functions"
+
+const Wrapper = styled.div`
+.edit {
+    display: ${({isEditView}) => !isEditView ? 'none' : 'initial'}};
+}
+`
+
+const LinkBox = styled(Link)`
+display: block;
+min-height: 430px;
+text-transform: capitalize;
+text-decoration: none;
+border-radius: 10px;
+box-shadow: 0px 0px 5px 0px black;
+`
+
+export const AdminMenu = ({isEditView }) => {
+    
+    const addLocationModal = () => {
+        addLocation({name: "test"})
+    }
+
+    return (
+    <Wrapper isEditView = {isEditView}>
+    <h1>Admin Menu</h1>
+    <button className="edit" onClick={addLocationModal}>Dodaj Lokalizacje</button>
+    <ul>
+        <li>Sklep 1 <button className="edit">edit</button></li>
+        <li>Sklep 2 <button className="edit">edit</button></li>
+        <li>Sklep 3 <button className="edit">edit</button></li>
+    </ul>
+ 
+    <LinkBox to='/AdminPanel'>Admin panel</LinkBox>
+    </Wrapper>
+    )
   }
