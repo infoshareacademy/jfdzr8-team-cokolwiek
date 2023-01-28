6a0232bb8112f5bf4af33e216b8c7c7b3af412b0
Gal Anonim <ssosnkrz@gmail.com>
2023-01-28 15:46:33 +0100

    x

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1,35 +1,20 @@
 import { useContext, useState, useEffect } from "react"
-import { usersInLocationOrderbyLastName } from "../firebase/utils/functions";
 import { MenuContent } from "./StateContainer"
-import { onSnapshot } from "@firebase/firestore";
+
 
 export const AdminPanel = () => {
   const context = useContext(MenuContent)
-  const [users, setUsers] = useState([])
 
-  useEffect(() => {
-    context.location.id ?
-    onSnapshot(usersInLocationOrderbyLastName(context.location.id), (querySnapshot) => {
-      const users = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setUsers(users);
-    }) : setUsers([])
-    
-  }, [context.location.id]);
 
     return (<>
     <h1>Admin Panel</h1>
     {context.location &&
     <>
     <div>selected location: {context.location.name}</div>
-    {users.map(user => {
-      if (user.location_id == context.location.id) {
-        //render users
-        console.log(context.location.id,user)
-      }
-    })}
+    {
+        console.log(context.location.id,context.users)
+      
+    }
     </>
     }
     </>
